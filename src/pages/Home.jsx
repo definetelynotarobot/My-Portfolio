0e24f14e3d4025c267a9ec025803c35c7c80b1a3
Mehmet Emin Eraslan <mehmetemineraslan7@gmail.com>
2025-04-14 16:47:23 +0300

    logout added aswell, axios addition

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,13 +9,18 @@ import { useAppContext } from '../context/AppContext'
 export default function Home() {
   const [name, setName] = useState('')
   const navigate = useNavigate() 
-  const { currentUser } = useAppContext()
+  const { currentUser,logout } = useAppContext()
 
   return (
     <div>
       <Input value={name} onChange={setName} />
       <Button text={`Hello ${name || 'World'}`} onClick={() => alert(`Hi, ${name || 'World'}!`)} />
-      <Button text={'Login / Signup'} onClick={() => navigate('/login')} disabled={!!currentUser}/> 
+      <Button
+        text={currentUser ? 'Already Logged In' : 'Login / Signup'}
+        onClick={() => navigate('/login')}
+        disabled={!!currentUser}
+      />
+      <Button text={'Logout'} onClick={logout}/>
     </div>
   )
 }
