04338af77548f0bb924c0d78482ad701f77c46e1
Mehmet Emin Eraslan <mehmetemineraslan7@gmail.com>
2025-04-14 18:42:58 +0300

    component {} typo lol

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import Button from '../components/Button/Button'
 import Input from '../components/Input/Input'
 import { useNavigate } from 'react-router-dom'
 import { useAppContext } from '../context/AppContext'
-import {Hero} from '../components/sections/Hero'
-import {Contact} from '../components/sections/Contact'
-import {ExperienceSteps} from '../components/sections/ExperienceSteps'
-import {SkillsSection} from '../components/sections/SkillsSection'
+import Hero from '../components/sections/Hero'
+import Contact from '../components/sections/Contact'
+import ExperienceSteps from '../components/sections/ExperienceSteps'
+import SkillsSection from '../components/sections/SkillsSection'
 
 
 
